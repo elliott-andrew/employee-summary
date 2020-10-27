b0ab57fc3c29163d4e12c5f1d41c43dc5323988b
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-10-26 18:08:19 -0600

    Able to log data from questions

--- Develop/app.js
+++ Develop/app.js
@@ -37,40 +37,43 @@ const questions = () => {
             choices: ["Manager", "Engineer", "Intern"],
             name: "employeeType",
         }
-    ])
+    ]).then(function (data) {
+        console.log(data.employeeType);
+    })
 
 }
 
-async function runPrompt() {
-    const response = await inquirer.prompt(questions)
-    if (response.employeeType === "Manager") {
-        inquirer.prompt([
-            {
-                type: "input",
-                message: "What is your office number?",
-                name: "officeNumber"
-            }
-        ])
-    } else if (response.employeeType === "Engineer") {
-        inquirer.prompt([
-            {
-                type: "input",
-                message: "What is your GitHub username?",
-                name: "github"
-            }
-        ])
-    } else if (response.employeeType === "Intern") {
-        inquirer.prompt([
-            {
-                type: "input",
-                message: "Which school are you attending?",
-                name: "school"
-            }
-        ])
-    }
-}
+questions();
+// async function runPrompt() {
+//     const response = await inquirer.prompt(questions)
+//     if (response.employeeType === "Manager") {
+//         inquirer.prompt([
+//             {
+//                 type: "input",
+//                 message: "What is your office number?",
+//                 name: "officeNumber"
+//             }
+//         ])
+//     } else if (response.employeeType === "Engineer") {
+//         inquirer.prompt([
+//             {
+//                 type: "input",
+//                 message: "What is your GitHub username?",
+//                 name: "github"
+//             }
+//         ])
+//     } else if (response.employeeType === "Intern") {
+//         inquirer.prompt([
+//             {
+//                 type: "input",
+//                 message: "Which school are you attending?",
+//                 name: "school"
+//             }
+//         ])
+//     }
+// }
 
-runPrompt();
+// runPrompt();
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
