c109b151651573cfd9cec5096ea0587f0d2421ff
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-10-26 18:32:16 -0600

    Added next employee prompt which renders the HTML once the user says there are no more new employees

--- Develop/app.js
+++ Develop/app.js
@@ -42,61 +42,64 @@ const questions = () => {
             inquirer.prompt([
                 {
                     type: "input",
-                    message: "What is the employee's office number?",
+                    message: "What is the managers's office number?",
                     name: "officeNumber"
                 }
             ]).then(function (managerData) {
                 let newManager = new Manager(data.name, data.id, data.email, managerData.officeNumber);
                 allEmployees.push(newManager);
+                nextEmployee();
             })
         }
         if (data.employeeType === "Engineer") {
             inquirer.prompt([
                 {
                     type: "input",
-                    message: "What is your GitHub username?",
+                    message: "What is the engineer's GitHub username?",
                     name: "github"
                 }
             ]).then(function (engineerData) {
                 let newEngineer = new Engineer(data.name, data.id, data.email, engineerData.github);
                 allEmployees.push(newEngineer);
-            })
+                nextEmployee();
+            });
         }
         if (data.employeeType === "Intern") {
             inquirer.prompt([
                 {
                     type: "input",
-                    message: "Which school are you attending?",
+                    message: "Which school is the intern attending?",
                     name: "school"
                 }
             ]).then(function (internData) {
                 let newIntern = new Intern(data.name, data.id, data.email, internData.school);
                 allEmployees.push(newIntern);
+                nextEmployee();
             })
         }
     })
 
 }
 
+const nextEmployee = () => {
+    inquirer.prompt([
+        {
+            type: "confirm",
+            message: "Would you like to add another employee?",
+            name: "add"
+        }
+    ]).then(function (data) {
+        if (data.add === true) {
+            questions();
+        } else {
+            fs.writeFile("team.html", render(allEmployees), function (err) {
+                if (err) {
+                    return console.log("Please try again.")
+                }
+            });
+            console.log("HTML file successfully created. Thank you!")
+        }
+    })
+}
+
 questions();
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ``
